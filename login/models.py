c33c3100856215a2f22ad3e19fba45a4dcf00db9
wxhnibr <1348671017@qq.com>
2021-12-26 23:49:09 +0800

    添加banji外键

--- login/models.py
+++ login/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from classpage.models import banji
 # Create your models here.
 class loginUser(models.Model):
     username = models.CharField(max_length=30)
@@ -7,6 +7,7 @@ class loginUser(models.Model):
     stu_name = models.CharField(max_length=30,null=True,blank=True)
     stu_id = models.CharField(max_length=30,null=True,blank=True)
     stu_school = models.CharField(max_length=30,null=True,blank=True)
+    banji = models.ForeignKey(banji,blank=True,related_name='banji',on_delete=models.CASCADE,null=True)
     def __str__(self):
         return self.username
 
