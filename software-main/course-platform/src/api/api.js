0f32cf310d5f17097ce9587814057ccc5b3582b4
WdFan <3480546229@qq.com>
2021-12-30 19:47:23 +0800

    添加dayjs计算时间戳

--- software-main/course-platform/src/api/api.js
+++ software-main/course-platform/src/api/api.js
@@ -5,7 +5,12 @@ const api = {
   login: (user_info) => { return axios.post('/api/login/', user_info) },
   signup: (user_info) => { return axios.post('/api/register/', user_info) },
   getTeachClass: (username) => { return axios.post('/api/getbanjiList/', {'username': username}) },
-  getStudyClass: (username) => { return axios.post('/api/getclassList/', {'username': username}) }
+  getStudyClass: (username) => { return axios.post('/api/getclassList/', {'username': username}) },
+  quiteClass: (username, classId) => {return axios.post('/api/quitclass/', {'username': username, 'classId': classId})},
+  getClassStudentInfo: (classId) => {return axios.post('/api/getclassStudentList/', {'classId': classId})},
+  deleteClass: (classId) => {return axios.post('/api/deleteClass/', {'classId': classId})},
+  deleteLesson: (lessonId) => {return axios.post('/api/deleteLesson/', {'lessonId': lessonId})},
+  getClassMessage: (classId) => {return axios.post('/api/getClassMessage/', {'classId': classId})}
 }
 
 export default api;
--- software-main/course-platform/src/main.js
+++ software-main/course-platform/src/main.js
@@ -4,10 +4,13 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import * as ElIconModules from '@element-plus/icons'
+import dayjs from 'dayjs'
 
 const app = createApp(App)
 installElementPlus(app)
 for (let iconName in ElIconModules) {
   app.component(iconName, ElIconModules[iconName])
 }
-app.use(store).use(router).mount('#app')
+app.config.globalProperties.$dayjs = dayjs
+app.use(store).use(router)
+app.mount('#app')
