f192cc920f5ccb28505f2c6ed8cc1ee6f5700345
WdFan <3480546229@qq.com>
2022-01-04 14:50:25 +0800

    最新需求

--- software-main/course-platform/src/api/api.js
+++ software-main/course-platform/src/api/api.js
@@ -8,15 +8,16 @@ const api = {
   getStudyClass: (username) => { return axios.post('/api/getclassList/', { 'username': username }) },
   createLesson: (username, createLessonForm) => { return axios.post('/api/createcourse/', { 'teacher': username, 'name': createLessonForm.lessonName, 'simple_name': createLessonForm.lessonSimpleName }) },
   createClass: (createClassForm) => { return axios.post('/api/createbanji/', { 'name': createClassForm.name, 'year': createClassForm.year, 'season': createClassForm.season, 'couid': createClassForm.lessonId, 'color': createClassForm.color }) },
-  editLesson: (lessonId, lessonForm) => {return axios.post('/api/editLesson/', {'lessonId': lessonId, 'lessonForm': lessonForm})},
-  editClass: (classId, classForm) => {return axios.post('/api/editClass/', {'classId': classId, 'classForm': classForm})},
+  editLesson: (lessonId, lessonForm) => { return axios.put('/api/editLesson/', { 'lessonId': lessonId, 'lessonForm': lessonForm }) },
+  editClass: (classId, classForm) => { return axios.post('/api/editClass/', { 'classId': classId, 'classForm': classForm }) },
   joinClass: (username, joinClassForm) => { return axios.post('/api/joinbanji/', { 'username': username, 'code': joinClassForm.classCode }) },
   quitClass: (username, classId) => { return axios.post('/api/quitclass/', { 'username': username, 'classId': classId }) },
   getClassStudentInfo: (classId) => { return axios.post('/api/getclassStudentList/', { 'classId': classId }) },
   deleteClass: (classId) => { return axios.post('/api/deleteClass/', { 'classId': classId }) },
   deleteLesson: (lessonId) => { return axios.post('/api/deleteLesson/', { 'lessonId': lessonId }) },
   getClassMessage: (classId) => { return axios.post('/api/getClassMessage/', { 'classId': classId }) },
-  savePersonInfo: (personInfoForm) => {return axios.post('/api/savePersonInfo/', { 'personInfoForm': personInfoForm})}
+  savePersonInfo: (personInfoForm) => { return axios.post('/api/savePersonInfo/', { 'personInfoForm': personInfoForm }) },
+  getClassInfo: (classId) => { return axios.post('/api/getClassInfo/', { 'classId': classId }) }
 }
 
 export default api;
